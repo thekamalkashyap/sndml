ac8b6383e6de3be2b46f7507f7c60420d518fcd8
thekamalkashyap <iamkamalkumar@proton.me>
2024-01-07 19:39:42 +0530

    success status

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const pass = process.env.SNDML_PASS;
 (async () => {
   if (!mail || !pass)
     return console.log(
-      "Environment variables not found.\n set SNDML_MAIL and SNDML_PASS in your environment."
+      "Environment variables not found.\nset SNDML_MAIL and SNDML_PASS in your environment."
     );
   const transporter = nodemailer.createTransport({
     service: "gmail",
@@ -17,10 +17,15 @@ const pass = process.env.SNDML_PASS;
       pass: pass,
     },
   });
-  await transporter.sendMail({
+  const info = await transporter.sendMail({
     from: mail,
     to: process.argv[2],
     subject: process.argv[3],
     text: process.argv[4],
   });
+  if(info.accepted){
+    console.log(`Email sent successfully to ${process.argv[2]}`);
+  }else{
+    console.log('Something went wrong. Try again later.');
+  }
 })();
